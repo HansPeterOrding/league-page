8b13ecc84a1682476ad51aec6890a19436ac2523
Björn May <bjoern.may.extern@lekkerland.com>
2021-08-03 16:56:21 +0200

    added my own info

--- src/routes/managers/managers.js
+++ src/routes/managers/managers.js
@@ -158,10 +158,10 @@ export const managers = [
     "roster": 8,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
     "name": "HansPeterOrding",
     "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    "location": "Brooklyn",
+    "location": "Eifel",
     "bio": "Lorem ipsum...",
     "photo": "/managers/everyone.png", // done
-    "fantasyStart": 2014, // when did the manager start playing fantasy football
+    "fantasyStart": 2020, // when did the manager start playing fantasy football
     "favoriteTeam": "gb", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": "Win Now", // 'Win Now' or 'Rebuild'
     "rival": {
@@ -169,11 +169,11 @@ export const managers = [
       link: 6, // manager array number within this array, or null to link back to all managers page
       image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
     },
-    "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    "favoritePlayer": 96, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
     "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
-    "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
-    "philosophy": "Your fantasy team's philosophy",
-    "tradingScale": 10, // 1 - 10
+    "rookieOrVets": "Vets", // 'Rookies' or 'Vets'
+    "philosophy": "Woran hat et jelejen?",
+    "tradingScale": 8, // 1 - 10
     "prefferedContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
   },
   {
